91ce1756369079dc426de44f21a761da7fde28d6
gishicrafter <gishicrafter@gmail.com>
2012-12-09 19:50:35 +0900

    Update and move ConfigHelper
    https://gist.github.com/4243552

--- src/colorwater/ModConfiguration.java
+++ src/colorwater/ModConfiguration.java
@@ -1,23 +1,24 @@
 package colorwater;
 
 import java.io.File;
+import gishicrafter.util.ConfigHelper;
 
 public class ModConfiguration
 {
-	@ConfigHelper.Item(name="bucketGeneric.id", defaultID=17650)
-	public static int idBucketGeneric;
+	@ConfigHelper.Item(name="bucketGeneric.id")
+	public static int idBucketGeneric=17650;
 	
-	@ConfigHelper.Item(name="liquid.id", defaultID=17651)
-	public static int idLiquid;
+	@ConfigHelper.Item(name="liquid.id")
+	public static int idLiquid=17651;
 	
-	@ConfigHelper.Item(name="bucketGel.id", defaultID=17652)
-	public static int idBucketGel;
+	@ConfigHelper.Item(name="bucketGel.id")
+	public static int idBucketGel=17652;
 	
-	@ConfigHelper.Item(name="itemGel.id", defaultID=17653)
-	public static int idItemGel;
+	@ConfigHelper.Item(name="itemGel.id")
+	public static int idItemGel=17653;
 	
-	@ConfigHelper.Block(name="gel.id", defaultID=3950)
-	public static int idBlockGel;
+	@ConfigHelper.Block(name="gel.id")
+	public static int idBlockGel=3950;
 	
 	public static void loadConfiguration(File file)
 	{
